428752c67afd8537f9ff3d9a68f73837f64edf91
Brian Picciano <bgp2396@bellsouth.net>
2012-11-17 01:43:10 -0500

    added in better documentation in the code

--- src/massage/json.clj
+++ src/massage/json.clj
@@ -37,38 +37,50 @@
 ; constraint, ensuring the value fits the constraint
 ; and possibly casting when necessary
 ;
-(defn process-type-error [keytype] {:error :wrong_type :should_be keytype})
+(defn process-type-error 
+    "Easily return an error for when given-value can't be casted to keytype" 
+    [keytype] {:error :wrong_type :should_be keytype})
 
-(defmulti process-option (fn [given-value option] 
-    (if (list? option) (first option)
-        option)))
+(defmulti process-option 
+    "Given a value and an option, attempt to match the value to the option.
+    If successful returns the (potentially massaged) value, otherwise returns
+    a map with the :error key set"
+    (fn [given-value option] 
+        (if (list? option) (first option)
+            option)))
 
 (defmethod process-option :string [given-value keytype]
+    "Tests for a string, possibly casts a number to a string"
     (cond (string? given-value) given-value
           (number? given-value) (str given-value)
           :else                 (process-type-error keytype)))
 
 (defmethod process-option :maxlength [given-value option]
+    "Makes sure a string or list isn't larger than a certain number"
     (if (> (count given-value) (last option))
         {:error :too_long :min_length (last option)}
         given-value))
 
 (defmethod process-option :minlength [given-value option]
+    "Makes sure a string or list isn't smaller than a certain number"
     (if (< (count given-value) (last option))
         {:error :too_short :min_length (last option)}
         given-value))
 
 (defmethod process-option :regex [given-value option]
+    "Makes sure a string matches a given regex"
     (if (nil? (re-find (last option) given-value))
         {:error :bad_string}
         given-value))
 
 (defmethod process-option :!regex [given-value option]
+    "Makes sure a string does not match a given regex"
     (if (nil? (re-find (last option) given-value))
         given-value
         {:error :bad_string}))
 
 (defmethod process-option :number [given-value keytype]
+    "Tests for a number, possibly casting a string to a number"
     (cond (number? given-value) given-value
           (string? given-value)
             (if (not (re-find #"-?[0-9]+\.?[0-9]*e?[0-9]*" given-value))
@@ -77,29 +89,38 @@
           :else                 (process-type-error keytype)))
 
 (defmethod process-option :max [given-value option]
+    "Makes sure a number isn't greater than a certain value"
     (if (> given-value (last option))
         {:error :too_big :max (last option)}
         given-value))
 
 (defmethod process-option :min [given-value option]
+    "Makes sure a number isn't less than a certain value"
     (if (< given-value (last option))
         {:error :too_small :min (last option)}
         given-value))
 
 (defmethod process-option :list   [given-value keytype]
+    "Tests for a list"
     (if (or (list? given-value) (vector? given-value))
         given-value
         (process-type-error keytype)))
 
 (defmethod process-option :object [given-value keytype]
+    "Tests for an object (represented by a map)"
     (if (map? given-value)
         given-value
         (process-type-error keytype)))
 
 (defmethod process-option :tpl [given-value option]
+    "Recursively call parse-json using the given-value and template given in
+    the option"
     (parse-json given-value (last option)))
 
 (defmethod process-option :map-tpl [given-value option]
+    "Call parse-json on each item in the list with the given template. If any
+    of these calls return an error this will forward that error directly, not
+    in the list"
     (let [mapped (map #(parse-json % (last option)) given-value)]
         (if-let [error (first (filter #(contains? % :error) mapped))]
             error
@@ -109,6 +130,12 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn check-all-options [given-value template-value]
+    "For each option in the template call process-option. If this returns
+    an error short-circuit and return that error. Otherwise continue on
+    and pass the return value to the next iteration of the loop. Since we
+    always start a template with a type, the casted value of that type
+    automatically gets sent into the process-option for the next option
+    in the options list"
     (loop [ current-value given-value
             option  (first template-value)
             options (rest template-value) ]
@@ -117,7 +144,13 @@
                 (if (contains? result :error) result
                     (recur result (first options) (rest options)))))))
 
-(defn check-data [tpl-key given-value template-value]
+(defn check-data 
+    "Given a key, the value for that key given in by the json (given-value) and
+    the template, check given-value with check-all-options. If the return is an
+    error return that error with :key set. Otherwise simply return the result.
+    If given-value is nil we assume it is not set in the json, which is only
+    allowed if the :optional option is given for this key in the template"
+    [tpl-key given-value template-value]
     (let [ keytype (first template-value)
            options (rest  template-value) ]
         (if (nil? given-value)
@@ -127,14 +160,24 @@
                 (if (and (contains? result :error) (not (contains? result :key)))
                     (assoc result :key tpl-key) result)))))
 
-(defmulti parse-json (fn [json-data template]
-    (if (map? template) :map
-                        :seq)))
+(defmulti parse-json
+    "Main entry point of this module. Given json-data and a template, returns
+    either a representation of the json data massaged to fit the template, or
+    a map with an :error key set if the data could not be massaged. See the 
+    README for more details on how to use templates"
+    (fn [json-data template]
+        (if (map? template) :map
+                            :seq)))
 
 (defmethod parse-json :seq [json-data options]
+    "If our second arg is a seq then we know it's an options and can be passed
+    straight through to check-all-options"
     (check-all-options json-data options))
 
 (defmethod parse-json :map [json-data template]
+    "If the second arg is a map then we go through each key and its value and send
+    them to check-data. If it returns an error we immediately return that error,
+    otherwise continue on to the next key/value in the template map"
     (loop [ tpl-seq template
             ret-map {} ]
         (let [ tpl-seq-tail (rest  tpl-seq)
