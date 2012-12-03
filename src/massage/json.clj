0eba347e88c61f450a30a3602c609880d9a0c625
Brian Picciano <bgp2396@bellsouth.net>
2012-12-02 20:24:29 -0500

    fixed bug in number regex

--- src/massage/json.clj
+++ src/massage/json.clj
@@ -83,7 +83,7 @@
     "Tests for a number, possibly casting a string to a number"
     (cond (number? given-value) given-value
           (string? given-value)
-            (if (not (re-find #"-?[0-9]+\.?[0-9]*e?[0-9]*" given-value))
+            (if (not (re-find #"^-?[0-9]+\.?[0-9]*e?[0-9]*$" given-value))
                 (process-type-error keytype)
                 (read-string given-value))
           :else                 (process-type-error keytype)))
